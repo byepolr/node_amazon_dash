1ca3fda6331beda29a7f544580ba05b14794e821
Jason Jones <jones.jason.w@gmail.com>
2018-02-25 16:58:20 -0800

    Add Tuya Device Support

--- app.js
+++ app.js
@@ -5,15 +5,22 @@ var config_file = (argv.config_file === undefined) ? './config.json' : argv.conf
 var config = require(config_file);
 var request = require('request');
 var dash_button = require('node-dash-button');
+var TuyaDevice = require('tuyapi');
+
 
 // These are actions that we may want to perform, as of now it's just the ifttt webhook
-var actions = {"ifttt_webhook": ifttt_webhook};
+var actions = {
+  "ifttt_webhook": ifttt_webhook,
+  "tuya_toggle": tuya_toggle
+};
 
 // Register dash buttons that we want to listen to
 dash_button_keys = Object.keys(config.buttons);
 var dash = dash_button(dash_button_keys, null, null, 'all');
 
-function ifttt_webhook(event_id, api_key){
+function ifttt_webhook(button){
+  event_id = button.id
+  api_key = button.ifttt_key
   url = "https://maker.ifttt.com/trigger/" + event_id + "/with/key/" + api_key;
   request(url, function (error, response, body) {
     if (!error && response.statusCode == 200) {
@@ -24,10 +31,25 @@ function ifttt_webhook(event_id, api_key){
   });
 }
 
+function tuya_toggle(button){
+  let tuya = new TuyaDevice({id: button.device_id, key: button.local_key});
+  tuya.resolveIds().then(() => {
+    tuya.get().then(status => {
+      tuya.set({set: !status}).then(result => {
+        tuya.get().then(status => {
+          msg = status ? "on" : "off"
+          console.log(button.name + ' is now ' + msg);
+          return;
+        });
+      });
+    });
+  });
+}
+
 dash.on("detected", function (dash_id){
   button = config.buttons[dash_id];
   if(button !== null){
     console.log("Detected dash button: " + button.name);
-    actions[button.action](button.id, config.ifttt_key);
+    actions[button.action](button);
   }
 });
